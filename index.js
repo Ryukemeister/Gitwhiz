be1e6b8204aaa1cd55db0cb7168c4da7382bb596
Ryuke <sahalrajiv-extc@atharvacoe.ac.in>
2023-02-16 17:57:18 +0530

    Show all commands in the index file

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello world");
+#!/usr/bin/env node
+
+const { program } = require("commander");
+
+program.helpOption(false);
+program.addHelpCommand(false);
+
+const suggest = require("./commands/suggestCmd");
+const elaborate = require("./commands/elaborateCmd");
+
+program.parse();
